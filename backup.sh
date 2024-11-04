311e9be3e876299c0fc5724f4d3fda2706914008
Matthew W. Thomas <mthomas@ftc.gov>
2024-11-04 08:45:40 -0500

    add rclone backup step

--- backup.sh
+++ backup.sh
@@ -13,3 +13,6 @@ find . -maxdepth 1 -type f -mtime +2 -exec rm {} \;
 
 # apply xz to compress all uncompressed backups
 find . -maxdepth 1 -type f ! -name '*.xz' -exec xz -z {} \;
+
+# upload to google drive
+rclone sync --transfers=8 --checkers=16 --drive-chunk-size=256M --retries=3 --low-level-retries=10 ./ gdrive:/website/backups/
