1ef5c701f34acfe2f91f26d590f9d3db4c56d8a3
Matthew W. Thomas <mthomas@ftc.gov>
2024-11-11 19:03:18 -0500

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -3,15 +3,15 @@
 # ensure the script exits on error
 set -e
 
-# get the filename of the most recent backup (they are named with a timestamp)
-latest_backup="$(find '/root/backups' -maxdepth 1 -type f -name '*.xz' -print | sort -r | head -n1)"
+# get the filename for selected file defaulting to the most recent backup (they are named with a timestamp)
+selected_backup="${1:-"$(find '/root/backups' -maxdepth 1 -type f -name '*.xz' -print | sort -r | head -n1)"}"
 
 # only restore if a backup was found
-if [ -n "$latest_backup" ]; then
+if [ -n "$selected_backup" ]; then
     # create a temporary file to hold the decompressed backup
     temp_file=$(mktemp -p /tmp/ -t webinoly_backup.XXXXXX)
     # decompress the backup
-    xz -dc "$latest_backup" >"$temp_file"
+    xz -dc "$selected_backup" >"$temp_file"
     # restore the webinoly configuration
     webinoly -backup=local -import=full -file="$temp_file"
     # remove the temporary file
