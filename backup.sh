8cb536d9ad80bfca4b0b9454ab6693d07fe912de
Matthew Thomas <matthew@mwt.me>
2025-08-25 18:27:26 -0400

    backup: no compress on n and safer backups

--- backup.sh
+++ backup.sh
@@ -8,13 +8,12 @@ cd /root/backups
 # backup the webinoly configuration
 webinoly -backup=local -export -destination=.
 
-# use find to delete all backups older than 3 days
-find . -maxdepth 1 -type f -mtime +2 -exec rm {} \;
+# if there are more than three files, use find to delete all backups older than 3 days
+if [ "$(find . -maxdepth 1 -type f -printf '1' | wc -c)" -gt 3 ]; then
+    find . -maxdepth 1 -type f -mtime +2 -exec rm {} \;
+fi
 
-# apply xz to compress all uncompressed backups
-find . -maxdepth 1 -type f ! -name '*.xz' -exec xz -z {} \;
-
-# upload to google drive unless -n option is set
+# compress all uncompressed backups and upload to google drive unless -n option is set
 while getopts 'n' opt; do
     case "${opt}" in
     n)
@@ -22,6 +21,9 @@ while getopts 'n' opt; do
         exit 0
         ;;
     *)
+        # apply xz to compress all uncompressed backups
+        find . -maxdepth 1 -type f ! -name '*.xz' -exec xz -z {} \;
+        # upload to google drive
         rclone copy --transfers=8 --checkers=16 --drive-chunk-size=256M --retries=3 --low-level-retries=10 ./ gdrive:/website/backups/
         ;;
     esac
