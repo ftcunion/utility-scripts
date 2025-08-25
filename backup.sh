aaeeb67d770d9e4022d4430098b8266b4daa3601
Matthew Thomas <matthew@mwt.me>
2025-08-25 18:11:54 -0400

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -14,5 +14,15 @@ find . -maxdepth 1 -type f -mtime +2 -exec rm {} \;
 # apply xz to compress all uncompressed backups
 find . -maxdepth 1 -type f ! -name '*.xz' -exec xz -z {} \;
 
-# upload to google drive
-rclone copy --transfers=8 --checkers=16 --drive-chunk-size=256M --retries=3 --low-level-retries=10 ./ gdrive:/website/backups/
+# upload to google drive unless -n option is set
+while getopts 'n' opt; do
+    case "${opt}" in
+    n)
+        echo "Skipping upload to Google Drive"
+        exit 0
+        ;;
+    *)
+        rclone copy --transfers=8 --checkers=16 --drive-chunk-size=256M --retries=3 --low-level-retries=10 ./ gdrive:/website/backups/
+        ;;
+    esac
+done
