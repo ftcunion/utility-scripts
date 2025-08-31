3cd61d5e272c9766c3401cacb8236a765cfb5e9e
Matthew Thomas <matthew@mwt.me>
2025-08-31 12:34:12 -0400

    try to use zbackup for backup and restore

--- backup.sh
+++ backup.sh
@@ -3,28 +3,48 @@
 # ensure the script exits on error
 set -e
 
-cd /root/backups
+current_month="$(date '+%Y-%m')"
+current_dtime="$(date +%F)-$(date +%T)"
+
+# if zbackup folder does not exist, create it
+if [ ! -d /root/zbackup ]; then
+    mkdir -p /root/zbackup
+fi
+
+cd /root/zbackup
+
+# we want one complete backup each month and incremental backups for each day
+if [ ! -d "$current_month" ]; then
+    # create current month directory
+    mkdir "$current_month"
+    # init zbackup for current month
+    zbackup init "$current_month" --non-encrypted
+fi
 
 # backup the webinoly configuration
-webinoly -backup=local -export -destination=.
+TEMP_FILE="$(mktemp /tmp/webinoly_full_backup.XXXXXX)"
+trap 'rm -f "$TEMP_FILE"' EXIT
+webinoly -backup=local -export -destination=/tmp -filename="$(basename "$TEMP_FILE")"
 
-# if there are more than three files, use find to delete all backups older than 3 days
-if [ "$(find . -maxdepth 1 -type f -printf '1' | wc -c)" -gt 3 ]; then
-    find . -maxdepth 1 -type f -mtime +2 -exec rm {} \;
-fi
+# run the webinoly backup through zbackup
+cat "$TEMP_FILE" | zbackup backup --non-encrypted "./$current_month/backups/webinoly_$current_dtime"
+
+# delete temporary file
+rm -f "$TEMP_FILE"
 
 # compress all uncompressed backups and upload to b2 unless -n option is set
 while getopts 'n' opt; do
     case "${opt}" in
     n)
-        echo "Skipping upload to Backblaze B2"
+        echo "Skipping cleanup and upload to Backblaze B2"
         exit 0
         ;;
     *) echo "Invalid option: ${opt}" ;;
     esac
 done
 
-# apply xz to compress all uncompressed backups
-find . -maxdepth 1 -type f ! -name '*.xz' -exec xz -z {} \;
 # upload to b2
 rclone copy --transfers 24 --checkers=48 --b2-chunk-size=60M --retries=3 --low-level-retries=10 ./ b2:webinoly-backups/
+
+# delete old months (they should be on b2)
+find . -maxdepth 1 -mindepth 1 -type d ! -name "$current_month" ! -name '.*' -exec rm -r {} \;
--- restore.sh
+++ restore.sh
@@ -3,20 +3,45 @@
 # ensure the script exits on error
 set -e
 
-# get the filename for selected file defaulting to the most recent backup (they are named with a timestamp)
-selected_backup="${1:-"$(find '/root/backups' -maxdepth 1 -type f -name '*.xz' -print | sort -r | head -n1)"}"
+# function to restore a tar archive from zbackup, returning filename
+restore_tar_archive() {
+	# exit if $1 is not defined
+	[ -z "$1" ] && exit 1
+
+	# variables
+	zbackup_path="$1"
+	output_path="/root/backups/$(basename "$zbackup_path")"
+
+	# restore the tar archive using zbackup
+	zbackup restore --non-encrypted "$zbackup_path" >"$output_path"
+	echo "$output_path"
+}
+
+# if $1 is specified and file says it is a tar archive, then use it
+if [ -n "$1" ] && [ "$(file -b --mime-type "$1")" = "application/x-tar" ]; then
+	selected_backup="$1"
+elif [ -n "$1" ]; then
+	selected_backup="$(restore_tar_archive "$1")"
+else
+	# try to guess!
+	# find the most recent month, (though having more than one month is an edge case)
+	most_recent_month="$(find '/root/zbackup' -maxdepth 1 -mindepth 1 -type d ! -name '.*' -print0 | sort -rz | head -z --lines=1 | tr -d '\0')"
+
+	# if most recent month is empty and no backup is specified, exit
+	if [ -z "$most_recent_month" ]; then
+		echo "No backup found to restore."
+		exit 1
+	fi
+
+	# get the filename for selected file defaulting to the most recent backup (they are named with a timestamp)
+	selected_zbackup="$(find "$most_recent_month/backups" -maxdepth 1 -type f ! -name '.*' -print0 | sort -rz | head -z --lines=1 | tr -d '\0')"
+
+	# otherwise, find the most recent backup
+	selected_backup="$(restore_tar_archive "$selected_zbackup")"
+fi
 
 echo "About to restore \"$selected_backup\". Press ENTER to continue or Ctrl-C to escape."
 read -r _
 
-# only restore if a backup was found
-if [ -n "$selected_backup" ]; then
-	# create a temporary file to hold the decompressed backup
-	temp_file=$(mktemp -p /tmp/ -t webinoly_backup.XXXXXX)
-	# decompress the backup
-	xz -dc "$selected_backup" >"$temp_file"
-	# restore the webinoly configuration
-	webinoly -backup=local -import=full -file="$temp_file"
-	# remove the temporary file
-	rm "$temp_file"
-fi
+# restore the webinoly configuration
+webinoly -backup=local -import=full -file="$selected_backup"
